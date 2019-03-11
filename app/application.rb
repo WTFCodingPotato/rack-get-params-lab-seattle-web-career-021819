aa7aafea0bfba99e4f166827d6d49b5012f3381e
Joshua M Loran <joshua.loran@gmail.com>
2019-03-11 02:22:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,6 +26,7 @@ class Application
       item = req.params("item")
       if @@items.include?(item)
         @@cart << item
+        resp.write ("")
     else
       resp.write "Path Not Found"
     end
