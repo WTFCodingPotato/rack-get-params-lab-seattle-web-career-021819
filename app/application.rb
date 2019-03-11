1c8c5aa512bf0ff5e0822c56a39befdaae45085c
Joshua M Loran <joshua.loran@gmail.com>
2019-03-11 02:19:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,6 +17,9 @@ class Application
     elsif req.path.match(/cart/)
       if @@cart.count == 0
         resp.write "Your cart is empty"
+      else 
+        @@cart.each do |item|
+          
     else
       resp.write "Path Not Found"
     end
